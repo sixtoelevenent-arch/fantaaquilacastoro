19426e38b170a469f35f17301de99a4084e233dc
sixtoelevenent-arch <sixtoelevenent@gmail.com>
2026-06-11 18:26:03 +0200

    Caricate rose ufficiali

--- data/rose.ts
+++ data/rose.ts
@@ -1,15 +1,25 @@
 export const rose = {
   Sirty: {
     nazione: "Francia",
-    portieri: [
-      "Fernando Muslera",
-      "G. Kobel",
-      "Sergio Rochet",
-    ],
-    attaccanti: [
-      "Erling Haaland",
-      "Mohamed Salah",
-      "Viktor Gyökeres",
-    ],
+    portieri: ["Fernando Muslera", "G. Kobel", "Sergio Rochet"],
+    attaccanti: ["Erling Haaland", "Mohamed Salah", "Viktor Gyökeres"],
+  },
+
+  Pres: {
+    nazione: "Pres",
+    portieri: [],
+    attaccanti: [],
+  },
+
+  "Andrea S.": {
+    nazione: "Ghana",
+    portieri: [],
+    attaccanti: [],
+  },
+
+  Cristian: {
+    nazione: "Messico",
+    portieri: [],
+    attaccanti: [],
   },
 };
