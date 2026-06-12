d74fce6425c968e14d779bb578f9d6ee82ee6014
sixtoelevenent-arch <sixtoelevenent@gmail.com>
2026-06-12 15:16:44 +0200

    Struttura dati centralizzata

---
+++ data/partecipanti.ts
@@ -0,0 +1,14 @@
+export const partecipanti = {
+  Portogallo: "Andrea A.",
+  Iran: "Pres",
+  Argentina: "Erny",
+  Colombia: "Luigi",
+  Francia: "Sirty",
+  Uzbekistan: "Michel",
+  Messico: "Cristian",
+  Curacao: "Fava",
+  Ghana: "Andrea S.",
+  Turchia: "Bruno",
+  "Costa d'Avorio": "Fabio",
+  Germania: "Martin",
+};
