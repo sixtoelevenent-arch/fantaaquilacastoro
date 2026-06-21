12025eb66c144b97d79e07a83cd0804ea2a2c59e
sixtoelevenent-arch <sixtoelevenent@gmail.com>
2026-06-21 06:46:19 +0200

    fix bug Import 3

--- lib/importVotes.ts
+++ lib/importVotes.ts
@@ -14,7 +14,7 @@ console.log(
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 const { data: activeMatchday, error: matchdayError } =
